d2062cd09cedd622a1586d74026b95b12bff97cf
Santosh Pingale <pingalesantosh@gmail.com>
2021-06-28 20:42:25 +0200

    Revert to previous implementation.

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
 # generate user files in the bootstrap
 
-useradd -m -d /home/octoprint -s /bin/bash  octoprint
+mkdir -p bootstrap/home/$1
+echo "$1:x:$USER_ID:$USER_ID:guest:/home/$1:/bin/sh" >> bootstrap/etc/passwd
