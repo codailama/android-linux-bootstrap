0559630bbbd385c1b980673d50888a2f50009f2d
Santosh Pingale <pingalesantosh@gmail.com>
2021-06-28 18:59:21 +0200

    Change user add script

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
 # generate user files in the bootstrap
 
-mkdir -p bootstrap/home/$1
-echo "$1:x:$USER_ID:$USER_ID:guest:/home/$1:/bin/sh" >> bootstrap/etc/passwd
+useradd -m -d /home/octoprint -s /bin/bash  octoprint
